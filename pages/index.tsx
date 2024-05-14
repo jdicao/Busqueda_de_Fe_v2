59cdc01ba35bfa8fcf40e0142711aa6b73d8bb38
jdicao <jeffdicao@gmail.com>
2024-05-14 13:39:05 -0500

    Actualizar imagen de busqueda de libros

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,7 +81,7 @@ export default function IndexPage() {
                 alt="Libros"
                 className="object-cover"
                 height={200}
-                src="./imagenes/libros_320.jpg"
+                src="./imagenes/libros_2_200.jpg"
                 width={200}
               />
               <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
