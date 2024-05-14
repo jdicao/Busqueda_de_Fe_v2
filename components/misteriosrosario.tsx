05352e11783fa23bcf6c49cb57d92bc82f2d5973
jdicao <jeffdicao@gmail.com>
2024-05-14 09:53:24 -0500

    completar misterios gozosos

--- components/misteriosrosario.tsx
+++ components/misteriosrosario.tsx
@@ -30,19 +30,11 @@ export default function MisteriosRosario() {
                     <div className="font-bold">
                       La Anunciación del Ángel a María
                     </div>
-                    El ángel Gabriel es enviado a Nazaret a una virgen llamada
-                    María, desposada con José. (v. 26-27) El ángel la saluda y
-                    le dice que ha hallado gracia ante Dios y que concebirá un
-                    hijo al que llamará Jesús. (v. 28) Jesús será grande e Hijo
-                    del Altísimo, y Dios le dará el trono de David, su padre.(v.
-                    32) María, perpleja, pregunta cómo puede ser esto si es
-                    virgen. (v. 34) El ángel le explica que el Espíritu Santo
-                    descenderá sobre ella y la cubrirá con su sombra, y por eso
-                    el Santo que nacerá será llamado Hijo de Dios. (v. 35) Le da
-                    como ejemplo a su pariente Isabel, que también está
-                    embarazada a pesar de su vejez. (v. 36) María, aceptando la
-                    voluntad de Dios, exclama: 'He aquí la sierva del Señor,
-                    hágase en mí según tu palabra. (v. 38)
+                    Al sexto mes, el ángel Gabriel fue enviado por Dios a una
+                    ciudad de Galilea llamada Nazaret, a una virgen desposada
+                    con un hombre llamado José, de la casa de David; el nombre
+                    de la virgen era María. Y entrando, le dijo: «Alégrate,
+                    llena de gracia, el Señor está contigo» (Lucas 1:26-28).
                   </CardBody>
                 </Card>
               </Tab>
@@ -52,38 +44,52 @@ export default function MisteriosRosario() {
                     <div className="font-bold">
                       La Visitación de María a su prima Isabel
                     </div>
-                    En cuanto Isabel oye el saludo de María, el niño salta de
-                    gozo en su vientre. (v. 41) Isabel, llena del Espíritu
-                    Santo, bendice a María y al fruto de su vientre, y llama a
-                    Jesús 'Señor'. (v. 42-43) María canta un cántico de alabanza
-                    a Dios por su misericordia y su poder. (v. 46-55) Isabel
-                    declara que María es bienaventurada por haber creído en la
-                    promesa de Dios. (v. 45) María permanece con Isabel unos
-                    tres meses y luego regresa a su casa. (v. 56)
+                    En aquellos días, María se levantó y se fue apresuradamente
+                    a la región montañosa, a una ciudad de Judá; entró en casa
+                    de Zacarías y saludó a Isabel. Y en cuanto Isabel oyó el
+                    saludo de María, el niño saltó de gozo en su seno, e Isabel
+                    quedó llena del Espíritu Santo (Lucas 1:39-41).
                   </CardBody>
                 </Card>
               </Tab>
               <Tab key="3" title="3ro">
                 <Card>
                   <CardBody>
-                    Tercero Excepteur sint occaecat cupidatat non proident, sunt
-                    in culpa qui officia deserunt mollit anim id est laborum.
+                    <div className="font-bold">
+                      El Nacimiento de Jesús en Belén
+                    </div>
+                    Y aconteció que, mientras estaban allí, se le cumplieron los
+                    días de su alumbramiento. Y dio a luz a su hijo primogénito,
+                    lo envolvió en pañales y lo acostó en un pesebre, porque no
+                    había lugar para ellos en la posada (Lucas 2:6-7).
                   </CardBody>
                 </Card>
               </Tab>
               <Tab key="4" title="4to">
                 <Card>
                   <CardBody>
-                    Cuarto Excepteur sint occaecat cupidatat non proident, sunt
-                    in culpa qui officia deserunt mollit anim id est laborum.
+                    <div className="font-bold">
+                      La Presentación de Jesús en el Templo
+                    </div>
+                    Cuando se cumplieron los días de la purificación de ellos,
+                    según la ley de Moisés, le trajeron a Jerusalén para
+                    presentarle al Señor, como está escrito en la ley del Señor:
+                    Todo varón primogénito será consagrado al Señor (Lucas
+                    2:22-23).
                   </CardBody>
                 </Card>
               </Tab>
               <Tab key="5" title="5to">
                 <Card>
                   <CardBody>
-                    Quinto Excepteur sint occaecat cupidatat non proident, sunt
-                    in culpa qui officia deserunt mollit anim id est laborum.
+                    <div className="font-bold">
+                      El Niño Jesús Perdido y Hallado en el Templo
+                    </div>
+                    Al verlo, se asombraron, y su madre le dijo: «Hijo, ¿por qué
+                    nos has hecho esto? Tu padre y yo te hemos estado buscando
+                    llenos de angustia.» Él les respondió: «¿Por qué me
+                    buscaban? ¿No sabían que tengo que estar en la casa de mi
+                    Padre?» (Lucas 2:48-49).
                   </CardBody>
                 </Card>
               </Tab>
